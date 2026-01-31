0f1e82a49e662dd085388c710df14160f7fd5447
waltal <waltalgg@yandex.ru>
2026-01-31 13:30:31 +0300

    feat: Компонентность

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+const navItems = {
+  timeline: ClockIcon,
+  activities: ListBulletIcon,
+  progress: ChartBarIcon
+  }
 </script>
 
 <template>
@@ -18,7 +25,19 @@ import { CheckCircleIcon } from '@heroicons/vue/24/solid'
       </div>
     </a>
   </header>
-  <main></main>
+  <main class="flex flex-col flex-grow">
+
+  </main>
+  <nav class="sticky bottom-0 z-10 bg-white">
+
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="(icon, page) in navItems" :key="page" class="flex-1">
+        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
+          <component :is="icon" class="h-6 w-6"/> {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped></style>
