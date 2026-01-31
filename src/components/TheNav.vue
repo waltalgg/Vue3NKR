41a90e5bbc93a9a5a10a36a71ba72fbd8116efd7
waltal <waltalgg@yandex.ru>
2026-01-31 14:45:25 +0300

    feat: Работа с константами и улучшение кода

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,22 +1,41 @@
 <script setup>
+import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavItem from '../components/NavItem.vue'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants'
 
 const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon,
+}
+
+const currentPage = ref(normalizePageHash())
+
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1)
+  console.log(hash)
+  if(Object.keys(navItems).includes(hash)) {
+    return hash
+  }
+
+  window.location.hash = PAGE_TIMELINE
+  return PAGE_TIMELINE
 }
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
-
     <ul class="flex items-center justify-around border-t">
-      <li v-for="(icon, page) in navItems" :key="page" class="flex-1">
-        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-          <component :is="icon" class="h-6 w-6"/> {{ page }}
-        </a>
-      </li>
+      <NavItem
+        v-for="(icon, page) in navItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
+        @:click="currentPage = page"
+      >
+        <component :is="icon" class="h-6 w-6" /> {{ page }}
+      </NavItem>
     </ul>
   </nav>
 </template>
